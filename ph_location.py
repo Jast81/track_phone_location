75b683ffe4445c24a76575e79625dfc668aaeeef
Jast81 <174167800+Jast81@users.noreply.github.com>
2024-07-16 02:36:58 -0400

    Update ph_location.py

--- ph_location.py
+++ ph_location.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import geocoder
-ph_num1 = phonenumbers.parse("+918247794171")
+ph_num1 = phonenumbers.parse("+17877189728")
 
 print("\nPhone Numbers Location\n")
 print(geocoder.description_for_number(ph_num1, "en"))
